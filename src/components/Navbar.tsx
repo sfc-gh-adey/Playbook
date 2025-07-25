1b0703869e853c2f42f84e95e208e42e50386f87
Developer <dev@example.com>
2025-07-25 15:45:51 -0700

    fix: Update Snowflake logo SVG to render correctly
    - Replace complex SVG paths with simpler chevron shapes
    - Use 200x200 viewBox for better precision
    - Add rounded corners to center diamond
    - Ensure all paths are properly closed and filled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,25 +11,33 @@ const Navbar: React.FC<NavbarProps> = ({ children }) => {
         {/* Left side - Logo and title */}
         <div className="flex items-center space-x-3">
           {/* Snowflake Logo */}
-          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g transform="translate(16, 16)">
-              {/* Center diamond */}
-              <rect x="-3" y="-3" width="6" height="6" transform="rotate(45 0 0)" fill="#29B5E8"/>
-              
-              {/* Top/Bottom arrows */}
-              <path d="M -4 -16 L 0 -10 L 4 -16 Z" fill="#29B5E8"/>
-              <path d="M -4 16 L 0 10 L 4 16 Z" fill="#29B5E8"/>
-              
-              {/* Left/Right arrows */}
-              <path d="M -16 -4 L -10 0 L -16 4 Z" fill="#29B5E8"/>
-              <path d="M 16 -4 L 10 0 L 16 4 Z" fill="#29B5E8"/>
-              
-              {/* Diagonal arrows */}
-              <path d="M -11.31 -11.31 L -7.07 -7.07 L -11.31 -2.83 Z" transform="rotate(-45 -9.19 -7.07)" fill="#29B5E8"/>
-              <path d="M 11.31 -11.31 L 7.07 -7.07 L 11.31 -2.83 Z" transform="rotate(45 9.19 -7.07)" fill="#29B5E8"/>
-              <path d="M -11.31 11.31 L -7.07 7.07 L -11.31 2.83 Z" transform="rotate(45 -9.19 7.07)" fill="#29B5E8"/>
-              <path d="M 11.31 11.31 L 7.07 7.07 L 11.31 2.83 Z" transform="rotate(-45 9.19 7.07)" fill="#29B5E8"/>
-            </g>
+          <svg width="32" height="32" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* Center diamond */}
+            <rect x="85" y="85" width="30" height="30" rx="2" transform="rotate(45 100 100)" fill="#29B5E8"/>
+            
+            {/* Top chevron */}
+            <path d="M100 10 L80 30 L90 40 L100 30 L110 40 L120 30 Z" fill="#29B5E8"/>
+            
+            {/* Right chevron */}
+            <path d="M190 100 L170 80 L160 90 L170 100 L160 110 L170 120 Z" fill="#29B5E8"/>
+            
+            {/* Bottom chevron */}
+            <path d="M100 190 L120 170 L110 160 L100 170 L90 160 L80 170 Z" fill="#29B5E8"/>
+            
+            {/* Left chevron */}
+            <path d="M10 100 L30 120 L40 110 L30 100 L40 90 L30 80 Z" fill="#29B5E8"/>
+            
+            {/* Top-right chevron */}
+            <path d="M160 40 L140 40 L140 50 L150 60 L140 70 L150 70 L160 60 Z" fill="#29B5E8"/>
+            
+            {/* Bottom-right chevron */}
+            <path d="M160 160 L160 140 L150 140 L140 150 L130 140 L130 150 L140 160 Z" fill="#29B5E8"/>
+            
+            {/* Bottom-left chevron */}
+            <path d="M40 160 L60 160 L60 150 L50 140 L60 130 L50 130 L40 140 Z" fill="#29B5E8"/>
+            
+            {/* Top-left chevron */}
+            <path d="M40 40 L40 60 L50 60 L60 50 L70 60 L70 50 L60 40 Z" fill="#29B5E8"/>
           </svg>
           
           {/* Title */}
